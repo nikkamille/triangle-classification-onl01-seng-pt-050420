b4ce8961f8261d51fa5a7951d2f0979cbf1b5ade
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-08 00:12:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,17 +21,12 @@ class Triangle
     if side1 <= 0 || side2 <= 0 || side3 <= 0
       begin 
         raise TriangleError
-      rescue TriangleError => error
-          puts error.message
+      
       end
     end
   end
   
   class TriangleError < StandardError
-    
-    def message
-      "This is not a triangle!"
-    end
     
   end
   
